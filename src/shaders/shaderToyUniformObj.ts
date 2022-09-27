3aeeb64f1128660039a9ad13dcae263f7cf3b05f
namitkapoor <51729833+namitkapoor@users.noreply.github.com>
2022-09-27 12:49:31 +0000

    Deploying to gh-pages from @ namitkapoor/core-components@7c95a34d90194baac7c2c584f49a5f3a78533ecf 🚀

--- src/shaders/shaderToyUniformObj.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    iTime: { value: 0.0 },
-    iResolution:  { value: new THREE.Vector3(512, 512, 1) },
-    texRepeat: { value: new THREE.Vector2(1,1) },
-    texOffset: { value: new THREE.Vector2(0,0) },
-    texFlipY: { value: 0 }
-};
--- src/shaders/shaderToyUniform_paras.ts
+++
@@ -1,7 +0,0 @@
-export default /* glsl */`
-uniform vec3 iResolution;
-uniform float iTime;
-uniform vec2 texRepeat;
-uniform vec2 texOffset;
-uniform int texFlipY; 
-  `;
